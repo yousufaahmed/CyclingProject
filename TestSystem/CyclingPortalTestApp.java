5af99f074d39d16e580b2ffa1ddeab3e958f62dd
Yousuf <yousufaa@outlook.com>
2024-02-22 16:27:36 +0000

    start code

---
+++ TestSystem/CyclingPortalTestApp.java
@@ -0,0 +1,52 @@
+import cycling.BadMiniCyclingPortalImpl;
+import cycling.IllegalNameException;
+import cycling.InvalidNameException;
+import cycling.MiniCyclingPortal;
+
+/**
+ * A short program to illustrate an app testing some minimal functionality of a
+ * concrete implementation of the CyclingPortal interface -- note you
+ * will want to increase these checks, and run it on your CyclingPortalImpl class
+ * (not the BadCyclingPortal class).
+ *
+ * 
+ * @author Diogo Pacheco
+ * @version 2.0
+ */
+public class CyclingPortalTestApp {
+
+	/**
+	 * Test method.
+	 * 
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		System.out.println("The system compiled and started the execution...");
+
+		// TODO replace BadMiniCyclingPortalImpl by CyclingPortalImpl
+		MiniCyclingPortal portal1 = new BadMiniCyclingPortalImpl();
+		MiniCyclingPortal portal2 = new BadMiniCyclingPortalImpl();
+
+		assert (portal1.getRaceIds().length == 0)
+				: "Innitial Portal not empty as required or not returning an empty array.";
+		assert (portal1.getTeams().length == 0)
+				: "Innitial Portal not empty as required or not returning an empty array.";
+
+		try {
+			portal1.createTeam("TeamOne", "My favorite");
+			portal2.createTeam("TeamOne", "My favorite");
+		} catch (IllegalNameException e) {
+			e.printStackTrace();
+		} catch (InvalidNameException e) {
+			e.printStackTrace();
+		}
+
+		assert (portal1.getTeams().length == 1)
+				: "Portal1 should have one team.";
+
+		assert (portal2.getTeams().length == 1)
+				: "Portal2 should have one team.";
+
+	}
+
+}
